c284e5c416e37a4a604dfd9f3832478f3cc12faa
Herve Senot <hsenot@gmail.com>
2012-07-26 15:23:00 +1000

    restUrl source configuration required to allow styling (if not, workspaced endpoint is used and refused)

--- src/app/app.js
+++ src/app/app.js
@@ -62,7 +62,7 @@ var gtProxy,gtLoginEndpoint,gtLocalLayerSourcePrefix;
 if (debugMode)
 {
 	gtProxy = "proxy/?url=";
-	gtLoginEndpoint = "http://localhost:8080/geoexplorer/login/";
+	gtLoginEndpoint = "http://new.pozi.com/geoexplorer/login/";
 	gtLocalLayerSourcePrefix = "http://new.pozi.com";
 }
 else
@@ -264,66 +264,6 @@ Ext.onReady(function() {
 		this.editAction.setDisabled(disable);
 		return disable;
 	};
-
-    /** private: method[checkIfStyleable]
-     *  :arg layerRec: ``GeoExt.data.LayerRecord``
-     *  :arg describeRec: ``Ext.data.Record`` Record from a 
-     *      `GeoExt.data.DescribeLayerStore``.
-     *
-     *  Given a layer record and the corresponding describe layer record, 
-     *  determine if the target layer can be styled.  If so, enable the launch 
-     *  action.
-     */
- 	// Reasons for override:
-	// - managing URL parameters that are arrays
-	// - managing workspaced layers
-	
-    gxp.plugins.Styler.prototype.checkIfStyleable = function(layerRec, describeRec) {
-        if (describeRec) {
-            var owsTypes = ["WFS"];
-            if (this.rasterStyling === true) {
-                owsTypes.push("WCS");
-            }
-        }
-        if (describeRec ? owsTypes.indexOf(describeRec.get("owsType")) !== -1 : !this.requireDescribeLayer) {
-            var editableStyles = false;
-            var source = this.target.layerSources[layerRec.get("source")];
-            var url;
-            // TODO: revisit this
-            var restUrl = layerRec.get("restUrl");
-            if (restUrl) {
-                url = restUrl + "/styles";
-            } else {
-//                url = source.url.split("?").shift().replace(/\/(wms|ows)\/?$/, "/rest/styles");
-		// Array of URLs
-		if (typeof source.url =="object"){ source.url = source.url[0];}
-		// Workspaced layers
-                url = source.url.split("?").shift().replace(/\/geoserver\/.*\/(wms|ows)\/?$/, "/geoserver/rest/styles");
-            }
-            if (this.sameOriginStyling) {
-                // this could be made more robust
-                // for now, only style for sources with relative url
-                editableStyles = url.charAt(0) === "/";
-                // and assume that local sources are GeoServer instances with
-                // styling capabilities
-                if (this.target.authenticate && editableStyles) {
-                    // we'll do on-demand authentication when the button is
-                    // pressed.
-                    this.launchAction.enable();
-                    return;
-                }
-            } else {
-                editableStyles = true;
-            }
-            if (editableStyles) {
-                if (this.target.isAuthorized()) {
-                    // check if service is available
-                    this.enableActionIfAvailable(url);
-                }
-            }
-        }
-    };
-
 
     /** private: method[createOutputConfig]
      *  :returns: ``Object`` Configuration object for an Ext.tree.TreePanel
