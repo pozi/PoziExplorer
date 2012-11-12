9b90081c5fc080c6026e146f744ac24bbdd66c1b
Chris Berkhout <chrisberkhout@gmail.com>
2012-11-12 17:53:56 +1100

    Reformatting.

--- src/app/onConfigurationLoaded.js
+++ src/app/onConfigurationLoaded.js
@@ -393,46 +393,16 @@ var onConfigurationLoaded = function() {
                 lga: gtLGACode
             },
             fields: [
-            {
-                name: "label",
-                mapping: "row.label"
-            },
-            {
-                name: "xmini",
-                mapping: "row.xmini"
-            },
-            {
-                name: "ymini",
-                mapping: "row.ymini"
-            },
-            {
-                name: "xmaxi",
-                mapping: "row.xmaxi"
-            },
-            {
-                name: "ymaxi",
-                mapping: "row.ymaxi"
-            },
-            {
-                name: "gsns",
-                mapping: "row.gsns"
-            },
-            {
-                name: "gsln",
-                mapping: "row.gsln"
-            },
-            {
-                name: "idcol",
-                mapping: "row.idcol"
-            },
-            {
-                name: "idval",
-                mapping: "row.idval"
-            },
-            {
-                name: "ld",
-                mapping: "row.ld"
-            }
+                { name: "label", mapping: "row.label" },
+                { name: "xmini", mapping: "row.xmini" },
+                { name: "ymini", mapping: "row.ymini" },
+                { name: "xmaxi", mapping: "row.xmaxi" },
+                { name: "ymaxi", mapping: "row.ymaxi" },
+                { name: "gsns",  mapping: "row.gsns" },
+                { name: "gsln",  mapping: "row.gsln" },
+                { name: "idcol", mapping: "row.idcol" },
+                { name: "idval", mapping: "row.idval" },
+                { name: "ld",    mapping: "row.ld" }
             ],
             proxy: new Ext.data.ScriptTagProxy({
                 url: gtSearchComboEndPoint
