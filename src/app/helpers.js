39fd90a8f4bc6541309f186a4a5a27bef719a81b
Chris Berkhout <chrisberkhout@gmail.com>
2012-11-14 14:29:01 +1100

    Reformatting.

--- src/app/helpers.js
+++ src/app/helpers.js
@@ -1,38 +1,33 @@
 // Helper functions
+
 function toTitleCase(str)
  {
-    return str.replace(/\w\S*/g, function(txt) {
-        return txt.charAt(0).toUpperCase() + txt.substr(1).toLowerCase();
-    });
+    return str.replace(
+        /\w\S*/g,
+        function(txt) {
+            return txt.charAt(0).toUpperCase() + txt.substr(1).toLowerCase();
+        }
+    );
 }
 
 function gotNum(str) {
     return /\d/.test(str);
 }
 
-function toSmartTitleCase(str)
- {
-    return str.replace(/\w\S*/g,
-    function(txt) {
-        if (gotNum(txt))
-        {
-            return txt;
-        }
-        else
-        {
+function toSmartTitleCase(str) {
+    return str.replace(
+        /\w\S*/g,
+        function(txt) {
+            if (gotNum(txt)) { return txt; }
             return txt.charAt(0).toUpperCase() + txt.substr(1).toLowerCase();
         }
-    });
+    );
 }
 
-function trim(str)
- {
-    if (str)
-    {
+function trim(str) {
+    if (str) {
         return str.replace(/^\s*/, "").replace(/\s*$/, "");
-    }
-    else
-    {
+    } else {
         return "";
     }
 }
