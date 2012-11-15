68ec62de51b5e2d82073dc6d8f2622e8f6d8b2ac
Chris Berkhout <chrisberkhout@gmail.com>
2012-11-15 11:23:52 +1100

    Simplified loop over JSONconf.liveDataEndPoints and removed unused temp ds.

--- src/app/onConfigurationLoaded.js
+++ src/app/onConfigurationLoaded.js
@@ -1548,52 +1548,49 @@ var onConfigurationLoaded = function(JSONconf, propertyDataInit) {
             var loadTabConfig = function() {
 
                 // Information panel layouts for the current authorized role - we should degrade nicely if the service is not found
-                var ds;
-                for (urlIdx in JSONconf.liveDataEndPoints) {
-                    if (JSONconf.liveDataEndPoints.hasOwnProperty(urlIdx)) {
-                        ds = new Ext.data.Store({
-                            autoLoad: true,
-                            proxy: new Ext.data.ScriptTagProxy({
-                                url: JSONconf.liveDataEndPoints[urlIdx].urlLayout
-                            }),
-                            reader: new Ext.data.JsonReader(
-                                {
-                                    root: 'rows',
-                                    totalProperty: 'total_rows',
-                                    id: 'key_arr'
-                                },
-                                [ { name: 'key_arr', mapping: 'row.key_arr' } ]
-                            ),
-                            baseParams: {
-                                role: gCurrentLoggedRole,
-                                mode: JSONconf.liveDataEndPoints[urlIdx].storeMode,
-                                config: JSONconf.liveDataEndPoints[urlIdx].storeName
+                _(JSONconf.liveDataEndPoints).each(function(endPoint) {
+                    new Ext.data.Store({
+                        autoLoad: true,
+                        proxy: new Ext.data.ScriptTagProxy({
+                            url: endPoint.urlLayout
+                        }),
+                        reader: new Ext.data.JsonReader(
+                            {
+                                root: 'rows',
+                                totalProperty: 'total_rows',
+                                id: 'key_arr'
                             },
-                            listeners: {
-                                load: function(store, recs) {
-                                    // Setting up a global variable array to define the info panel layouts
-                                    for (key = 0; key < recs.length; key++) {
-                                        var a = recs[key].json.row.val_arr;
+                            [ { name: 'key_arr', mapping: 'row.key_arr' } ]
+                        ),
+                        baseParams: {
+                            role: gCurrentLoggedRole,
+                            mode: endPoint.storeMode,
+                            config: endPoint.storeName
+                        },
+                        listeners: {
+                            load: function(store, recs) {
+                                // Setting up a global variable array to define the info panel layouts
+                                for (key = 0; key < recs.length; key++) {
+                                    var a = recs[key].json.row.val_arr;
 
-                                        if (gLayoutsArr[recs[key].json.row.key_arr]) {
-                                            // If this key (layer) already exists, we add the JSON element (tab) to its value (tab array)
-                                            gLayoutsArr[recs[key].json.row.key_arr] = gLayoutsArr[recs[key].json.row.key_arr].concat(a);
-                                            // Reordering the array elements inside the array for this key, according to orderNum
-                                            gLayoutsArr[recs[key].json.row.key_arr].sort(function(a, b) {
-                                                return parseInt(a.orderNum) - parseInt(b.orderNum);
-                                            });
-                                        } else {
-                                            // We create this key if it didn't exist
-                                            gLayoutsArr[recs[key].json.row.key_arr] = a;
-                                        }
+                                    if (gLayoutsArr[recs[key].json.row.key_arr]) {
+                                        // If this key (layer) already exists, we add the JSON element (tab) to its value (tab array)
+                                        gLayoutsArr[recs[key].json.row.key_arr] = gLayoutsArr[recs[key].json.row.key_arr].concat(a);
+                                        // Reordering the array elements inside the array for this key, according to orderNum
+                                        gLayoutsArr[recs[key].json.row.key_arr].sort(function(a, b) {
+                                            return parseInt(a.orderNum) - parseInt(b.orderNum);
+                                        });
+                                    } else {
+                                        // We create this key if it didn't exist
+                                        gLayoutsArr[recs[key].json.row.key_arr] = a;
                                     }
-
-                                    add_default_tabs();
                                 }
+
+                                add_default_tabs();
                             }
-                        });
-                    }
-                };
+                        }
+                    });
+                });
             };
 
             // Loading the tabs on initial page load
