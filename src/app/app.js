29bf224a357f459832ea0fa4f98bdad71e8379e1
Chris Berkhout <chrisberkhout@gmail.com>
2013-06-17 01:37:26 +1000

    Added new Google Analytics tracking code for whole app.

--- src/app/app.js
+++ src/app/app.js
@@ -113,6 +113,16 @@ var gtyp = { value: undefined }; // feature type (e.g Address)
 var glab = { value: undefined }; // feature label (e.g. 12 Acorn Lane Packenham 3810)
 var gtLayerLabel = { value: undefined };
 
+// Google Analytics - start
+(function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){
+(i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),
+m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)
+})(window,document,'script','//www.google-analytics.com/analytics.js','ga');
+
+ga('create', 'UA-27881682-2', 'pozi.com');
+ga('send', 'pageview');
+// Google Analytics - end
+
 Ext.onReady(function() {
     requestConfig({
         onLoad: function(clientConfig, propertyDataInit) {
