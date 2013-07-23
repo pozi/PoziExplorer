9b95a82612ddf396a14ac90c02d685fc50e88ea3
Herve Senot <hsenot@gmail.com>
2013-07-23 17:51:20 +1000

    Fixing possible integers passed to helper functions - using toString

--- src/app/helpers.js
+++ src/app/helpers.js
@@ -9,22 +9,44 @@ helpers = function() {
     return {
 
         toTitleCase: function(str) {
-            return str.replace(
-                /\w\S*/g,
-                function(txt) {
-                    return txt.charAt(0).toUpperCase() + txt.substr(1).toLowerCase();
+            if (str)
+            {
+                if (typeof str !== 'string')
+                {
+                    str = str.toString();
                 }
-            );
+                return str.replace(
+                    /\w\S*/g,
+                    function(txt) {
+                        return txt.charAt(0).toUpperCase() + txt.substr(1).toLowerCase();
+                    }
+                );
+            }
+            else
+            {
+                return "";
+            }
         },
 
         toSmartTitleCase: function(str) {
-            return str.replace(
-                /\w\S*/g,
-                function(txt) {
-                    if (gotNum(txt)) { return txt; }
-                    return txt.charAt(0).toUpperCase() + txt.substr(1).toLowerCase();
+            if (str)
+            {
+                if (typeof str !== 'string')
+                {
+                    str = str.toString();
                 }
-            );
+                return str.replace(
+                    /\w\S*/g,
+                    function(txt) {
+                        if (gotNum(txt)) { return txt; }
+                        return txt.charAt(0).toUpperCase() + txt.substr(1).toLowerCase();
+                    }
+                );
+            }
+            else
+            {
+                return "";
+            }
         },
 
         trim: function(str) {
