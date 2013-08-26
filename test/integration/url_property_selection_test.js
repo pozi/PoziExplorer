0ae10e6a3826638f4f0c7eea4180bbd6c39f10c1
Chris Berkhout <chrisberkhout@gmail.com>
2013-08-26 11:21:39 +0200

    Adjusted test to actually test property selection via URL.

--- test/integration/url_property_selection_test.js
+++ test/integration/url_property_selection_test.js
@@ -13,11 +13,15 @@ describe("URL property selection", function(){
   });
 
   it("should work", function(done){
+    var detailsTab = '//div[@id="gtAccordion"]//div[@id="attributeAcc"]';
+    var addressValueDiv = detailsTab + '//td/div[text()="Address"]/../../td[last()]/div';
 
     browser
-    .get("http://www.google.com")
-    .title(function(err, title) {
-      expect(title).to.equal("Googz");
+    .get("http://localhost:9090/?config=cardinia&property=3755100500")
+    // .waitForElementByXPath('//div[text()="11 SAVAGE STREET PAKENHAM 3810"]', 10000, function(err) { expect(err).to.be.a('null'); done(); });
+    .waitForElementByXPath(detailsTab, 10000, function(err) { expect(err).to.be.a('null'); })
+    .elementByXPath(addressValueDiv, function(err, element) {
+      element.text(function(err,text) { expect(text).to.equal('11 SAVAGE STREET PAKENHAM 3810'); done(); });
     });
     
   });
