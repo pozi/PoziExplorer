054cf50f912b0dd89277916befc086dedda75d24
Chris Berkhout <chrisberkhout@gmail.com>
2013-08-26 17:29:04 +0200

    Fixed TravisCI stuff (it's using a different version of PhantomJS/GhostDriver.

--- test/bin/start_for_local.sh
+++ test/bin/start_for_local.sh
@@ -1,12 +1,12 @@
 #!/bin/bash
 
-exec 9< <(phantomjs --webdriver='127.0.0.1:8192')
-grep -m 1 "Ghost Driver running on port" <&9
-exec 9<&-
+exec 3< <(phantomjs --webdriver='127.0.0.1:8192')
+grep -m 1 "running on port" <&3
+exec 3<&-
 
-exec 9< <(./suite-sdk debug -l 9090 -g http://general.pozi.com/geoserver .)
-grep -m 1 "Server on http://localhost:9090 started" <&9
-exec 9<&-
+exec 3< <(./suite-sdk debug -l 9090 -g http://general.pozi.com/geoserver .)
+grep -m 1 "Server on http://localhost:9090 started" <&3
+exec 3<&-
 
 export POZIEXPLORER_TEST_WEBDRIVER="http://localhost:8192/wd/hub"
 export POZIEXPLORER_TEST_SUBJECT="http://localhost:9090/"
