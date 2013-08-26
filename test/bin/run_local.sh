0fce06b6558356382e8d37a9913cd7f9f24b987b
Chris Berkhout <chrisberkhout@gmail.com>
2013-08-26 17:13:05 +0200

    Revert "Debugging travis setup."
    This reverts commit e78fa804262c6b3f982c4acb91e703e604c827ab.

--- test/bin/run_local.sh
+++ test/bin/run_local.sh
@@ -1,24 +1,6 @@
 #!/bin/bash
 
-# source ./test/bin/start_for_local.sh
-
-
-# exec 3< <(phantomjs --webdriver='127.0.0.1:8192')
-# grep -m 1 "Ghost Driver running on port" <&3
-# exec 3<&-
-
-# exec 3< <(./suite-sdk debug -l 9090 -g http://general.pozi.com/geoserver .)
-# grep -m 1 "Server on http://localhost:9090 started" <&3
-# exec 3<&-
-
-phantomjs --webdriver='127.0.0.1:8192' &
-./suite-sdk debug -l 9090 -g http://general.pozi.com/geoserver . &
-
-sleep 10
-
-export POZIEXPLORER_TEST_WEBDRIVER="http://localhost:8192/wd/hub"
-export POZIEXPLORER_TEST_SUBJECT="http://localhost:9090/"
-
+source ./test/bin/start_for_local.sh
 
 mocha
 result=$?
