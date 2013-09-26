f1a635079e5bd23a6708648cf45685761454aa72
Chris Berkhout <chrisberkhout@gmail.com>
2013-09-26 12:34:11 +1000

    Added test for background layers being displayed below others.

--- test/integration/url_property_selection.js
+++ test/integration/url_property_selection.js
@@ -1,5 +1,6 @@
 var expect = require("chai").expect,
-    wd = require("wd");
+    wd = require("wd"),
+    _ = require("underscore");
 
 if (!("POZIEXPLORER_TEST_WEBDRIVER" in process.env) || !("POZIEXPLORER_TEST_SUBJECT" in process.env)) {
   throw new Error("Env vars not set!");
@@ -37,3 +38,31 @@ describe("URL property selection", function(){
 
 });
 
+describe("Layer presentation order", function(){
+  var browser;
+
+  beforeEach(function() {
+    browser = wd.remote(process.env.POZIEXPLORER_TEST_WEBDRIVER).chain().init();
+  });
+  afterEach(function() {
+    browser.quit();
+  });
+
+  it("should put the background layers before layers of any other group", function(done){
+    var emptyBasemapInLayerTree = '//li[@class="x-tree-node"]/div/a/span[text()="None"]';
+
+    browser
+      .get(process.env.POZIEXPLORER_TEST_SUBJECT + "?config=mitchell")
+      .waitForElementByXPath(emptyBasemapInLayerTree, timeout_for(this))
+      .waitForConditionInBrowser('try { typeof app.mapPanel.layers.data.items === \'object\' } catch(e) { false }')
+      .safeEval('_(app.mapPanel.layers.data.items).map(function(i){ return i.data.group; })', function(err, result) {
+        expect(err).to.be.a('null');
+        var layersWithGroup = _(result).compact();
+        var startToLastBackground = _(layersWithGroup).first(_(layersWithGroup).lastIndexOf('background')+1);
+        expect(_(startToLastBackground).uniq()).to.have.length(1);
+        done();
+      });
+    
+  });
+
+});
