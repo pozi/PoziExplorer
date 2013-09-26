af25074bdaa59f79a5f27844460b59df346d4f98
Chris Berkhout <chrisberkhout@gmail.com>
2013-09-26 14:55:53 +1000

    Pretty printed json config (no change in content).

---
+++ lib/custom/json/transform.rb
@@ -0,0 +1,11 @@
+#!/usr/bin/env ruby
+
+require "json"
+
+Dir.glob("*.json").each do |file|
+  puts file
+  data = JSON.parse(File.read(file))
+  # data['layers'] = data['layers'].reverse
+  IO.write file, JSON.pretty_generate(data, indent: "    ")
+end
+
