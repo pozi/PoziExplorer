0787aaee90ab7e5f7b40d2124f5b6c859b079098
Chris Berkhout <chrisberkhout@gmail.com>
2013-09-26 14:59:48 +1000

    Reversed layer order in config (still need to fix code).

--- lib/custom/json/transform.rb
+++ lib/custom/json/transform.rb
@@ -5,7 +5,7 @@ require "json"
 Dir.glob("*.json").each do |file|
   puts file
   data = JSON.parse(File.read(file))
-  # data['layers'] = data['layers'].reverse
+  data['layers'] = data['layers'].reverse
   IO.write file, JSON.pretty_generate(data, indent: "    ")
 end
 
