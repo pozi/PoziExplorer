55b39756e06dcdbe7193c5bc54246ed7cdce0204
Herve Senot <herve.senot@groundtruth.com.au>
2014-04-29 12:11:36 +1000

    Added WMTS as a printable layer type

--- src/app/overrides/gxp/plugins/Print.js
+++ src/app/overrides/gxp/plugins/Print.js
@@ -251,6 +251,7 @@ gxp.plugins.Print = Ext.extend(gxp.plugins.Tool, {
             function isSupported(layer) {
                 return (
                     layer instanceof OpenLayers.Layer.WMS ||
+                    layer instanceof OpenLayers.Layer.WMTS ||
                     layer instanceof OpenLayers.Layer.Vector ||
                     layer instanceof OpenLayers.Layer.OSM
                 );
