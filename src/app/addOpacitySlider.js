24ba84af2c18280cc6b7142b3f1a871badc5ed5c
Herve Senot <herve.senot@groundtruth.com.au>
2014-07-22 14:24:39 +1000

    Minor adjustement on drop down length

--- src/app/addOpacitySlider.js
+++ src/app/addOpacitySlider.js
@@ -18,7 +18,7 @@ addOpacitySlider = function(app) {
         {
             if (layerData[j].val)
             {
-                currentValWidth = layerData[j].val.length * 8;
+                currentValWidth = layerData[j].val.length * 8 + 30;
                 if (currentValWidth > calculatedWidth)
                 {
                     calculatedWidth = currentValWidth;
